56d8d783c276ccf62fb6cdec74a626cc5e7c0bcd
OLLI17 <wertersat351@mail.ru>
2025-09-29 20:49:57 +0300

    Создал интерфейс Removable

---
+++ lablab2/src/main/java/functions/Removable.java
@@ -0,0 +1,13 @@
+package functions;
+
+/**
+ * Интерфейс для удаления элементов по индексу
+ */
+public interface Removable {
+    /**
+     * Удаляет элемент по указанному индексу
+     * @param index индекс удаляемого элемента
+     * @throws IllegalArgumentException если индекс выходит за границы списка
+     */
+    void remove(int index);
+}
