bd49febbef94d5622c5a8f18a00a069bb566f642
Aloha1324 <sergey.dudkin.2005@mail.ru>
2025-10-26 15:53:14 +0400

    исправлена проблема вывода в консоль

--- lablab2/src/main/java/concurrent/ReadTask.java
+++ lablab2/src/main/java/concurrent/ReadTask.java
@@ -1,21 +1,27 @@
 package concurrent;
 
 
+
 import functions.TabulatedFunction;
 
-public class ReadTask implements Runnable {
-    private final TabulatedFunction function;
+public class ReadTask implements Runnable
+{
+    private TabulatedFunction function;
+    private final Object lock;
 
-    public ReadTask(TabulatedFunction function) {
+    public ReadTask(TabulatedFunction function, Object lock) {
         this.function = function;
+        this.lock = lock;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < function.getCount(); i++) {
-            double x = function.getX(i);
-            double y = function.getY(i);
-            System.out.printf("After read: i = %d, x = %f, y = %f%n", i, x, y);
+            synchronized (lock) {
+                double x = function.getX(i);
+                double y = function.getY(i);
+                System.out.printf("After read: i = %d, x = %f, y = %f%n", i, x, y);
+            }
         }
     }
 }
