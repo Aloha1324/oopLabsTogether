a3d2bf04d52e71c25f7ca013828ca5cbdf1524c9
OLLI17 <wertersat351@mail.ru>
2025-11-06 17:41:28 +0300

    Исправления

--- lablab2/src/main/java/concurrent/SynchronizedTabulatedFunction.java
+++ lablab2/src/main/java/concurrent/SynchronizedTabulatedFunction.java
@@ -10,22 +10,17 @@ import java.util.NoSuchElementException;
 public class SynchronizedTabulatedFunction implements TabulatedFunction {
     private final TabulatedFunction function;
 
-    // Исправленный конструктор - убрано поле lock
     public SynchronizedTabulatedFunction(TabulatedFunction function) {
         this.function = function;
     }
 
-    // Интерфейс для операций, выполняемых в синхронизированном контексте
     public interface Operation<T> {
         T apply(SynchronizedTabulatedFunction func);
     }
 
-    // Метод для выполнения произвольных операций в синхронизированном контексте
     public synchronized <T> T doSynchronously(Operation<? extends T> operation) {
         return operation.apply(this);
     }
-
-    // Синхронизированные методы доступа к данным функции
 
     @Override
     public synchronized int getCount() {
@@ -73,32 +68,32 @@ public class SynchronizedTabulatedFunction implements TabulatedFunction {
     }
 
     @Override
-    public synchronized Iterator<Point> iterator() {
-        // Создаем копию точек в синхронизированном блоке
-        Point[] pointsCopy = TabulatedFunctionOperationService.asPoints(function);
+    public Iterator<Point> iterator() {
+        synchronized (function) {
+            Point[] copyPoints = TabulatedFunctionOperationService.asPoints(function);
 
-        // Возвращаем итератор, работающий с копией данных (не требует синхронизации)
-        return new Iterator<Point>() {
-            private int currentIndex = 0;
-            private final Point[] points = pointsCopy;
+            return new Iterator<Point>() {
+                private int currentIndex = 0;
+                private final Point[] points = copyPoints;
 
-            @Override
-            public boolean hasNext() {
-                return currentIndex < points.length;
-            }
-
-            @Override
-            public Point next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException("No more elements in iterator");
+                @Override
+                public boolean hasNext() {
+                    return currentIndex < points.length;
                 }
-                return points[currentIndex++];
-            }
 
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException("Remove operation is not supported");
-            }
-        };
+                @Override
+                public Point next() {
+                    if (!hasNext()) {
+                        throw new NoSuchElementException("No more elements in iterator");
+                    }
+                    return points[currentIndex++];
+                }
+
+                @Override
+                public void remove() {
+                    throw new UnsupportedOperationException("Remove operation is not supported");
+                }
+            };
+        }
     }
 }
