9ee4e5ea79429cd14cfa8554f00c3b2e0c13c46b
OLLI17 <wertersat351@mail.ru>
2025-11-16 16:06:32 +0300

    Revert "Логирование"
    This reverts commit d88fd10b

--- lablab2/src/main/java/concurrent/MultiplyingTask.java
+++ lablab2/src/main/java/concurrent/MultiplyingTask.java
@@ -1,23 +1,37 @@
 package concurrent;
 
 import functions.TabulatedFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MultiplyingTask implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(MultiplyingTask.class);
     private final TabulatedFunction function;
 
     public MultiplyingTask(TabulatedFunction function) {
+        if (function == null) {
+            logger.error("Attempt to create MultiplyingTask with null function");
+            throw new IllegalArgumentException("Function cannot be null");
+        }
         this.function = function;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < function.getCount(); i++) {
-            synchronized (function) {
-                double currentY = function.getY(i);
-                function.setY(i, currentY * 2);
-            }
-        }
+        try {
+            logger.info("Starting multiplication task for {} points", function.getCount());
 
-        System.out.println("Поток " + Thread.currentThread().getName() + " закончил выполнение задачи");
+            for (int i = 0; i < function.getCount(); i++) {
+                synchronized (function) {
+                    double currentY = function.getY(i);
+                    function.setY(i, currentY * 2);
+                }
+            }
+
+            logger.info("Multiplication task completed successfully");
+        } catch (Exception e) {
+            logger.error("Error during multiplication task execution", e);
+            throw e;
+        }
     }
 }
