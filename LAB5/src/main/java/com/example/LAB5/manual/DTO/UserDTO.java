351128552757bb690016e3e1e01f55b271930ffc
Aloha1324 <sergey.dudkin.2005@mail.ru>
2025-12-01 10:16:35 +0400

    Создал единый API для manual ветки

--- LAB5/src/main/java/com/example/LAB5/manual/DTO/UserDTO.java
+++ LAB5/src/main/java/com/example/LAB5/manual/DTO/UserDTO.java
@@ -4,21 +4,21 @@ import java.util.Objects;
 
 public class UserDTO {
     private Long id;
-    private String login;
+    private String username;
     private String role;
     private String password;
 
     public UserDTO() {}
 
-    public UserDTO(String login, String role, String password) {
-        this.login = login;
+    public UserDTO(String username, String role, String password) {
+        this.username = username;
         this.role = role;
         this.password = password;
     }
 
-    public UserDTO(Long id, String login, String role, String password) {
+    public UserDTO(Long id, String username, String role, String password) {
         this.id = id;
-        this.login = login;
+        this.username = username;
         this.role = role;
         this.password = password;
     }
@@ -27,8 +27,8 @@ public class UserDTO {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getLogin() { return login; }
-    public void setLogin(String login) { this.login = login; }
+    public String getLogin() { return username; }
+    public void setLogin(String username) { this.username = username; }
 
     public String getRole() { return role; }
     public void setRole(String role) { this.role = role; }
@@ -42,20 +42,20 @@ public class UserDTO {
         if (o == null || getClass() != o.getClass()) return false;
         UserDTO userDTO = (UserDTO) o;
         return Objects.equals(id, userDTO.id) &&
-                Objects.equals(login, userDTO.login) &&
+                Objects.equals(username, userDTO.username) &&
                 Objects.equals(role, userDTO.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, login, role);
+        return Objects.hash(id, username, role);
     }
 
     @Override
     public String toString() {
         return "UserDTO{" +
                 "id=" + id +
-                ", login='" + login + '\'' +
+                ", username='" + username + '\'' +
                 ", role='" + role + '\'' +
                 '}';
     }
