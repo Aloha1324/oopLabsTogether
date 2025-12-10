703dec77975bca6815a35f011b6dd7cdbba3faab
Aloha1324 <sergey.dudkin.2005@mail.ru>
2025-12-10 15:38:35 +0400

    AuthService

--- LAB5/src/main/java/com/example/LAB5/manual/DAO/UserDAO.java
+++ LAB5/src/main/java/com/example/LAB5/manual/DAO/UserDAO.java
@@ -1,14 +1,12 @@
 package com.example.LAB5.manual.DAO;
 
+import com.example.LAB5.manual.DTO.UserDTO;
 import com.example.LAB5.manual.config.DatabaseConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class UserDAO {
     private static final Logger logger = LoggerFactory.getLogger(UserDAO.class);
