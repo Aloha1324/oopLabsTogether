c38ec812acaff97f181dbf8c71d61ec4985487cb
Aloha1324 <sergey.dudkin.2005@mail.ru>
2025-12-16 12:35:03 +0400

    f

--- LAB7/src/main/java/com/example/LAB5/framework/controller/HomeController.java
+++ LAB7/src/main/java/com/example/LAB5/framework/controller/HomeController.java
@@ -2,10 +2,11 @@ package com.example.LAB5.framework.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     // ✅ ГЛАВНАЯ → JWT авторизация (index.html)
